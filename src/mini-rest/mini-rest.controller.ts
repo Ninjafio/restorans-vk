4cc23cea9dcae95005edbeeb1585df29944a1190
Art Perch <art.perchatkin@gmail.com>
2025-04-05 17:18:52 +0500

    0.1.2

--- src/mini-rest/mini-rest.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('mini-rest')
-export class MiniRestController {}
--- src/mini-rest/mini-rest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MiniRestService } from './mini-rest.service';
-import { MiniRestController } from './mini-rest.controller';
-
-@Module({
-  providers: [MiniRestService],
-  controllers: [MiniRestController]
-})
-export class MiniRestModule {}
